96f7d1cd01f9c085373b0a4a9578843d39241245
Fima <FimaRozovsky@hotmail.com>
2018-05-28 10:50:17 +0300

    Built the skeleton structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LibraryComponent } from './library/library.component';
+import { BookComponent } from './library/book/book.component';
+import { EditModalComponent } from './library/edit-modal/edit-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibraryComponent,
+    BookComponent,
+    EditModalComponent
   ],
   imports: [
     BrowserModule
